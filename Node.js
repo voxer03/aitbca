98a757b0619846e23fe4edd0a56cd503dd0a9c6c
Divij Jain <68986989+diiviij@users.noreply.github.com>
2020-10-18 09:47:59 +0000

    Update Node.js

--- Node.js
+++ Node.js
@@ -1,10 +1,13 @@
-var http = require('http');
-var fs = require('fs');
-http.createServer(function (req, res) {
-  //Open a file on the server and return its content:
-  fs.readFile('extracourse.html', function(err, data) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+var url = require('url');
+var adr = 'http://aitbca.netlify.app/extracourse';
+//Parse the address:
+var q = url.parse(adr, true);
+
+/*The parse method returns an object containing url properties*/
+console.log(q.host);
+console.log(q.pathname);
+console.log(q.search);
+
+/*The query property returns an object with all the querystring parameters as properties:*/
+var qdata = q.query;
+console.log(qdata.month);
